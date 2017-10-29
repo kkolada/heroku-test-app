f0cb407b7ef746a3ead1762851ab04c88c69c85d
Krzysztof Kolada <krzysztof.kolada@gmail.com>
2017-10-30 00:20:27 +0100

    Routing for dashboard added, active links on navbar fixed, space between logo and links on navbar shrank, animation for navbar links added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {RouterModule, Routes} from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {BestEmployeesWidget} from './components/best-employees-widget/best-employees-widget';
-import {UserListComponent} from './components/user-list/user-list.component';
+import { AppComponent } from './app.component';
+import { BestEmployeesWidget } from './components/best-employees-widget/best-employees-widget';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-import {UserService} from "./services/user.service";
-import {AddUserComponent} from './components/add-user/add-user.component';
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {EditUserComponent} from './components/edit-user/edit-user.component';
-import {StorageService} from "./services/storage.service";
+import { UserService } from './services/user.service';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { StorageService } from './services/storage.service';
 import { StarRatingComponent } from './shared/star-rating/star-rating.component';
 
 
 const appRoutes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     component: DashboardComponent
   },
   {
@@ -32,6 +32,11 @@ const appRoutes: Routes = [
   {
     path: 'app-edit-user/:userId',
     component: EditUserComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
   }
 ];
 
