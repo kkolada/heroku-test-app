a053d6be7c0371ab6eeb65e31fad4017db3382e8
Krzysztof Kolada <krzysztof.kolada@gmail.com>
2017-10-29 23:42:06 +0100

    Node server (express) with configuration for Heroku.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+
+// Gzip
+app.use(compression());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
